5e9622101bbcc09efd0cd4bc84a619d554d15ba7
brian kanari <briangitui26@gmail.com>
2024-07-29 20:17:02 +0300

    adds solution

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
-
+    counter = 10
+    while counter > 0:
+       print (counter)
+       counter -= 1
+       print("Happy New Year!")
+    
 def square_integers(int_list):
+    return [i ** 2 for i in int_list]
     # code goes here!
-    pass
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
